7a4d2679c7b63891585d12c5e6822eeb5dd58c14
jleon11 <justinleon1111@gmail.com>
2026-05-07 12:24:20 -0600

    ajustesss

--- lib/constants/constantes_sistema.dart
+++ lib/constants/constantes_sistema.dart
@@ -211,7 +211,7 @@ class SistemaConstantes {
   }
 
   static double obtenerCardAncho(double width) {
-    if (width < mobile) return 170;
+    if (width < mobile) return 200;
     if (width < tablet) return 240;
     return 300;
   }
